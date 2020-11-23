a699a241e901b98f6e6268999b99d4465540ccb7
vieraboschkova <58532638+vieraboschkova@users.noreply.github.com>
2020-11-23 23:51:22 +0100

    feat: edge case

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,54 @@
-const summin = require('./index');
+const filteringWith = require('./index');
+const arrayOfObjects = require('./testObject')
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(summin(1, 2)).toBe(3);
+test('checks if filter is long enough', () => {
+  expect(filteringWith(arrayOfObjects, 'a')).toStrictEqual([]);
+});
+
+test('checks if filter is long enough', () => {
+  expect(filteringWith(arrayOfObjects, '')).toStrictEqual([]);
+});
+
+test('checks if filter is long enough', () => {
+  expect(filteringWith(arrayOfObjects, 12)).toStrictEqual([]);
+});
+
+test('checks if search works in the first level', () => {
+  expect(filteringWith(arrayOfObjects, 26)).toStrictEqual([{
+    "_id": "5e985a0737e2306e9aef6ecd",
+    "age": 26,
+    "eyeColor": "blue",
+    "name": "Mcguire Mercado",
+    "gender": "male",
+    "company": "LINGOAGE",
+    "email": "mcguiremercado@lingoage.com",
+    "phone": "+1 (963) 450-2194",
+    "tags": [
+      "cupidatat",
+      "occaecat",
+      "amet",
+      "qui",
+      "elit",
+      "esse",
+      "deserunt"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Loraine Harper"
+      },
+      {
+        "id": 1,
+        "name": "Luann Randall"
+      },
+      {
+        "id": 2,
+        "name": "Obrien Rich"
+      },
+      {
+        "id": 3,
+        "name": "Noble Wilkerson"
+      }
+    ]
+  }]);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-function sum(a: number, b:number):number {
-    return a + b;
+function filterWith(arr: Object[], filter: string | number): Object {
+  let foundObjects: Object[] = [];
+
+  const filterLength: number = typeof filter === 'string' ? 
+    filter.length 
+    : 
+    filter.toString().length;
+  const filterTooShort = filterLength < 3;
+
+  if (filterTooShort) {
+    return [];
   }
-module.exports = sum;
+
+  return foundObjects;
+}
+
+function checkIfNested(objectToSearch: Object, level: number,  ...rest: any) {
+  if (objectToSearch === undefined) return false
+  if (rest.length == 0 && obj.hasOwnProperty(level)) return true
+  return checkIfNested(objectToSearch[level], ...rest)
+}
+
+module.exports = filterWith;
