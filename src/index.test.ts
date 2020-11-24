2e966eb8c6baa266a25b3c5d97abb0f68124fbeb
VieraBoschkova <vieraboschkova@gmail.com>
2020-11-24 21:59:00 +0100

    feat: implement filterWith

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,54 +1,191 @@
-const filteringWith = require('./index');
-const arrayOfObjects = require('./testObject')
-
-test('checks if filter is long enough', () => {
-  expect(filteringWith(arrayOfObjects, 'a')).toStrictEqual([]);
-});
-
-test('checks if filter is long enough', () => {
-  expect(filteringWith(arrayOfObjects, '')).toStrictEqual([]);
-});
-
-test('checks if filter is long enough', () => {
-  expect(filteringWith(arrayOfObjects, 12)).toStrictEqual([]);
-});
-
-test('checks if search works in the first level', () => {
-  expect(filteringWith(arrayOfObjects, 26)).toStrictEqual([{
-    "_id": "5e985a0737e2306e9aef6ecd",
-    "age": 26,
-    "eyeColor": "blue",
-    "name": "Mcguire Mercado",
-    "gender": "male",
-    "company": "LINGOAGE",
-    "email": "mcguiremercado@lingoage.com",
-    "phone": "+1 (963) 450-2194",
+const arrayForTesting: Object[] = [
+  {
+    "_id": "5e985a07feddae7617ac44f6",
     "tags": [
-      "cupidatat",
-      "occaecat",
-      "amet",
-      "qui",
+      "labore",
       "elit",
-      "esse",
-      "deserunt"
     ],
     "friends": [
       {
-        "id": 0,
+        "name": "Sheppard Jensen"
+      }
+    ]
+  },
+  {
+    "_id": "5e985a0709dfa1e6fd93c6ad",
+    "tags": [
+      "nisi",
+      "veniam",
+    ],
+    "friends": [
+      {
+        "name": "Bruce Barton"
+      },
+      {
+        "name": "Juliet Schmidt"
+      },
+      {
+        "id": 2447,
+        "name": "Horton Haley"
+      },
+      {
+        "id": 3777,
+        "name": "Herminia Witt"
+      }
+    ]
+  },
+  {
+    "_id": "5e985a0737e2306e9aef6ecd",
+    "age": 26777,
+    "tags": [
+      "cupidatat",
+      "occaecat",
+    ],
+    "friends": [
+      {
+        "id": 5770,
         "name": "Loraine Harper"
       },
       {
-        "id": 1,
+        "id": 1858,
         "name": "Luann Randall"
       },
+    ]
+  },
+  {
+    "_id": "5e985a07148cfba58c860ec2",
+    "age": 26444,
+    "phone": "+1 (867) 417-3497",
+    "tags": [
+      "laborum",
+      "aliquip",
+    ],
+    "friends": [
       {
-        "id": 2,
-        "name": "Obrien Rich"
+        "name": "Blair Hill"
       },
       {
-        "id": 3,
-        "name": "Noble Wilkerson"
+        "id": 17575,
+        "name": "Ebony Jimenez"
       }
     ]
-  }]);
+  },
+  {
+    "_id": "5e985a074984f9f08ccaaa4c",
+    "age": 25,
+    "email": "barlowferguson@toyletry.com",
+    "phone": "+1 (837) 484-2231",
+    "tags": [
+      "est",
+      "dolor",
+    ],
+    "friends": [
+      {
+        "id": 575750,
+        "name": "Delacruz Acevedo"
+      },
+      {
+        "id": 1,
+        "name": "Gloria Tanner"
+      },
+    ]
+  }
+]
+const filteringWith = require('./index');
+
+test('checks if filter is long enough', () => {
+  expect(filteringWith(arrayForTesting, 'a')).toStrictEqual([]);
 });
+
+test('checks if filter is long enough', () => {
+  expect(filteringWith(arrayForTesting, '')).toStrictEqual([]);
+});
+
+test('checks if filter is long enough', () => {
+  expect(filteringWith(arrayForTesting, 12)).toStrictEqual([]);
+});
+
+test('checks if filter is of type string or number', () => {
+  expect(()=> {
+    filteringWith(arrayForTesting, [12])}).toThrow(TypeError);
+});
+
+test('checks if array is of type array', () => {
+  expect(()=> {
+    filteringWith({a: undefined}, 123)}).toThrow(TypeError);
+});
+
+test('checks if array is not empty', () => {
+  expect(()=> {
+    filteringWith([], 123)}).toThrow(TypeError);
+});
+
+test('checks if array contains only valid objects', () => {
+  expect(()=> {
+    filteringWith([{a: undefined}, null], 123)}).toThrow(TypeError);
+});
+
+test('checks if search works with numerical filter', () => {
+  expect(filteringWith(arrayForTesting, 575)).toStrictEqual([
+    {
+      "_id": "5e985a07148cfba58c860ec2",
+      "age": 26444,
+      "phone": "+1 (867) 417-3497",
+      "tags": [
+        "laborum",
+        "aliquip",
+      ],
+      "friends": [
+        {
+          "name": "Blair Hill"
+        },
+        {
+          "id": 17575,
+          "name": "Ebony Jimenez"
+        }
+      ]
+    },
+    {
+      "_id": "5e985a074984f9f08ccaaa4c",
+      "age": 25,
+      "email": "barlowferguson@toyletry.com",
+      "phone": "+1 (837) 484-2231",
+      "tags": [
+        "est",
+        "dolor",
+      ],
+      "friends": [
+        {
+          "id": 575750,
+          "name": "Delacruz Acevedo"
+        },
+        {
+          "id": 1,
+          "name": "Gloria Tanner"
+        },
+      ]
+    }
+  ])
+});
+
+
+test('checks if search works with string filter', () => {
+  expect(filteringWith(arrayForTesting, 'eppa')).toStrictEqual([
+    {
+      "_id": "5e985a07feddae7617ac44f6",
+      "tags": [
+        "labore",
+        "elit",
+      ],
+      "friends": [
+        {
+          "name": "Sheppard Jensen"
+        }
+      ]
+    }
+  ])
+})
+
+test('checks if search works only with values, not keys', () => {
+  expect(filteringWith(arrayForTesting, 'end')).toStrictEqual([])
+})
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,78 @@
-function filterWith(arr: Object[], filter: string | number): Object {
-  let foundObjects: Object[] = [];
+// korzystając z funkcji .filter stwórz funkcję filterWith(arr, filter) 
+// która ma umożliwić przeszukanie arraya z obiektami 
+// po wszystkich wartościach obiektu typu string lub number
+// także po wartościach arraya w kluczu "tags" oraz po "name" w kluczu "friends"
+// tak aby:
+// - od 0 do 2 znaków w filtrze zwracało pusty array, 
+// - a powyżej 2 ma filtrować po każdej wartości typu string lub number w obiekcie
 
+function filterWith(arr: Array<object>, filter): Object {
+  // CHECK IF INPUT ARGUMENTS ARE CORRECT
+  const filterIsANumberOrString = typeof filter === 'string' || typeof filter === 'number';
+  const givenArrayIsAnArray = Array.isArray(arr);
+  const givenArrayIsEmpty = arr.length === 0;
+  const givenArrayContainsOnlyObjects = arr.every(checkedObject => {
+    if (checkedObject === null || typeof checkedObject === 'function') {
+      return false;
+    }
+    return typeof checkedObject === 'object'
+  });
   const filterLength: number = typeof filter === 'string' ? 
-    filter.length 
-    : 
-    filter.toString().length;
+    filter.length : filter.toString().length;
   const filterTooShort = filterLength < 3;
 
+  if (!filterIsANumberOrString) {
+    throw new TypeError('Change filter parameters to string or number');
+  }
+  if (!givenArrayIsAnArray) {
+    throw new TypeError('Enter a valid array');
+  }
+  if (givenArrayIsEmpty) {
+    throw new TypeError('Array is empty, nothing to filter');
+  }
+  if (!givenArrayContainsOnlyObjects) {
+    throw new TypeError('Array items are invalid');
+  }
   if (filterTooShort) {
     return [];
   }
+  // INITIATE ARRAY OF FILTERED OBJECTS TO RETURN
+  let foundObjects;
+
+  foundObjects = arr.filter((item) => {
+    return checkValues(item, filter);
+  })
 
   return foundObjects;
 }
 
-function checkIfNested(objectToSearch: Object, level: number,  ...rest: any) {
-  if (objectToSearch === undefined) return false
-  if (rest.length == 0 && obj.hasOwnProperty(level)) return true
-  return checkIfNested(objectToSearch[level], ...rest)
+// FUNCTION TO GET VALUES FROM OBJECT AND CHECK IF AT LEAST ONE OF THE VALUES CONTAINS THE FILTER PHRASE
+function checkValues(searchObject: object, searchPhrase) {
+  const isAString = (checkedValue: string): boolean => typeof checkedValue === ('string');
+  const isANumber = (checkedValue: number): boolean => typeof checkedValue === ('number');
+  const isAnObject = (checkedValue: object): boolean => typeof checkedValue === ('object'); 
+  const convertNumberToString = (checkedValue: number): string => checkedValue.toString();
+  let hasSearchedPhrase: boolean;
+  let allValues = Object.values(searchObject);
+  
+  hasSearchedPhrase = allValues.some((value) => {
+      if (isAString(value)) {
+          if (value.includes(searchPhrase)) {
+              return true;
+          }
+      }
+      if (isANumber(value)) {
+          let numericValueAsAString: string = convertNumberToString(value);
+          if (numericValueAsAString.includes(searchPhrase)) {
+              return true;
+          }
+      } 
+      if (isAnObject(value)) {
+          return checkValues(value, searchPhrase)
+      } 
+  })
+
+  return hasSearchedPhrase;
 }
 
 module.exports = filterWith;
